ce56d824798ac3717836b904ec1eddf1ff61cb52
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-03-19 12:23:01 +0100

    up

---
+++ ruiter-home.php
@@ -0,0 +1,71 @@
+<?php
+
+
+//Template name: Vacaturepogina
+
+get_header();?>
+
+<section class="content">
+    <div class="content__container">
+        <div class="content__left">
+            <div class="content__left--wrapper">
+
+                <!-- Bovenste bar -->
+                <div class="content__left--top">
+                    <div class="content__left--item">
+                        <img src="<?php echo get_template_directory_uri();?>/content/img/1.png" width="50px"/>
+                        <div class="content__left--item-info">
+                            <span>Dienstverband</span>
+                            <p>Loondienst of ZZP</p>
+                        </div>
+                    </div>
+                    <div class="content__left--item">
+                        <img src="<?php echo get_template_directory_uri();?>/content/img/2.png" width="36px"/>
+                        <div class="content__left--item-info">
+                            <span>Regio</span>
+                            <p>Utrecht/N-Z Holland</p>
+                        </div>
+                    </div>
+                    <div class="content__left--item">
+                        <img src="<?php echo get_template_directory_uri();?>/content/img/3.png" width="32px"/>
+                        <div class="content__left--item-info">
+                            <span>Team</span>
+                            <p>Alleen/met je maat</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Main content -->
+                <div class="content__left--main">
+                    <div class="content__left--summary">
+                        <h4>Als Dakkapel Monteur bij Ruiter:</h4>
+                        <h3>
+                            Heb je een <strong>stoere baan op grote hoogte</strong></br>
+                            Kom elke dag <strong>bij klanten thuis</strong></br>
+                            Ervaar je <strong>veel afwisseling</strong> in je werkzaamheden</br>
+                            Ben je met z'n tweeen een <strong>hecht team</strong></br>
+                            Krijg je <strong>alle ruimte om te groeien</strong></br>
+                            Kan je in <strong>loondiesnt of als ZZP'er</strong> aan de slag
+                        </h3>
+                    </div>
+                    <div class="content__left--bottom">
+                        <section>
+                            <p>Als grootste dakkapelspecialist in Nederland staan wij voor kwaliteit en betrouwbaarheid. Voor onze Dakkapel Monteurs geldt dat ook. Wij zoeken daarom ervaren vakmannen. Vakmannen die onze klanten – die een gat in hun dak stiekem best spannend vinden – blij maken met een dakkapel waar zij vaak al maanden naar uitkijken. Vakmannen die samen met ons willen bouwen aan de toekomst. En dat op grote hoogtes. </p>
+                        </section>
+                        <section>
+                            <h4>Laat jouw jongensdroom uitkomen</h4>
+                            <p>Jij droomde toen je klein was toch ook van afwisselend werk dat ertoe doet? Bij Ruiter Dakkapellen komt deze droom uit! Als Dakkapel Monteur ben jij specialist op het gebied van dakkapellen plaatsen. Je weet precies wat je doet, werkt nauwkeurig en bent oplossingsgericht. Ook de communicatie naar klanten gaat jou soepel af. Je stelt klanten op hun gemak en vertelt hoe deze – voor hen spannende – dag gaat verlopen. De combinatie van een vakkundig geplaatste dakkapel en tevreden klanten maakt jou trots!</p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="content__right">
+
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer();?>

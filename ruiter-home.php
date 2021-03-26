e3d81ff77750a59b394a83924b0f04625f99751d
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-03-26 10:57:29 +0100

    asd

--- ruiter-home.php
+++ ruiter-home.php
@@ -127,8 +127,8 @@ get_header();?>
                 <h4>Niets voor jou, wel voor een maat?</h4>
                 <p>Laat het ze weten!</p>
                 <div class="content__right--share-buttons">
-                    <a href="whatsapp://send?text=Bekijk deze vacature!"><img src="<?php echo get_template_directory_uri();?>/content/img/whatsapp-brands.svg" alt="Whatsapp" width="20px"></a>
-                    <a href="mailto:?subject=Bekijk deze vacature!&amp;body=Bekijk de vacature voor dakkapel monteur! http://www.website.com."><img src="<?php echo get_template_directory_uri();?>/content/img/envelope-solid.svg" alt="Mail" width="20px"></a>
+                    <a href="whatsapp://send?text=Dit is echt iets voor jou maat! Bekijk deze vacature!"><img src="<?php echo get_template_directory_uri();?>/content/img/whatsapp-brands.svg" alt="Whatsapp" width="20px"></a>
+                    <a href="mailto:?subject=Dit is echt iets voor jou maat! Bekijk deze vacature!&amp;body=Bekijk de vacature voor dakkapel monteur! http://www.website.com."><img src="<?php echo get_template_directory_uri();?>/content/img/envelope-solid.svg" alt="Mail" width="20px"></a>
                 </div>
             </div>
         </div>

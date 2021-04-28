a0f0edb0a687798bcaa99ac74e34f2cbf3945569
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro.local>
2021-04-28 16:37:43 +0200

    Added eventlistener for a form submit, send gtag event on submit

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $(function() {
         $('#cvuploaden').hide();
     });
 });
+
+console.log('Contact form GA listener loaded - Brut Communicatie');
+//Add event listener to a  succesful form submission
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    //Send the information to Google Analytics
+    // ga( 'send', 'event', 'Contact Form', 'submit' ); 
+    gtag('event', 'submit_form');
+  }, false );
